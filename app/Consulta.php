8dc465e8cb5051e6bd3c384b1c0a1831f3ef3608
pb0c <p.boc89@gmail.com>
2020-06-18 18:55:41 -0400

    .

--- app/Consulta.php
+++ app/Consulta.php
@@ -8,17 +8,12 @@ class Consulta extends Model
 {
     public function paciente()
     {
-<<<<<<< HEAD
         return $this->belongsTo(Paciente::class); //
-=======
-        return $this->belongsTo(Paciente::class);
->>>>>>> d7f6f0d8eaefb3bc967a072729cc375a4502cc13
     }
     public function empleado()
     {
         return $this->belongsTo(Empleado::class);
     }
-<<<<<<< HEAD
 
     protected $date = [
         'fecha'
@@ -26,8 +21,6 @@ class Consulta extends Model
     
     //formato fecha
     protected $casts = [
-        'fecha' => 'datetime:d-m-Y',
+        'fecha' => 'datetime:d-m-Y'
     ];
-=======
->>>>>>> d7f6f0d8eaefb3bc967a072729cc375a4502cc13
 }
--- app/Paciente.php
+++ app/Paciente.php
@@ -24,44 +24,7 @@ class Paciente extends Model
     
     //formato fecha
     protected $casts = [
-        'nacimiento' => 'datetime:d-m-Y',
+        'nacimiento' => 'datetime:d-m-Y'
     ];
 
-<<<<<<< HEAD
-    /*Consulta
-    public function paciente()
-    {
-        return $this->belongsTo(Paciente::class); //
-    }
-    public function empleado()
-    {
-        return $this->belongsTo(Empleado::class);
-    }
-
-    protected $date = [
-        'fecha'
-    ];
-    
-    //formato fecha
-    protected $casts = [
-        'fecha' => 'datetime:d-m-Y',
-    ];
-
-    
-    Schema::create('consultas', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->time('hora');
-            $table->date('fecha');
-            $table->integer('box');
-            $table->integer('numSesion');
-            //FK
-            $table->unsignedBigInteger('paciente_id'); //relación con paciente
-            $table->foreign('paciente_id')->references('id')->on('paciente'); //FK desde paciente
-            $table->unsignedBigInteger('empleado_id'); //relación con empleado
-            $table->foreign('empleado_id')->references('id')->on('empleado');
-            $table->timestamps();
-        }); */
-
-=======
->>>>>>> d7f6f0d8eaefb3bc967a072729cc375a4502cc13
 }
